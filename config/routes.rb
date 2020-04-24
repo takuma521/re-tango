99436c073ca71e71863a64cc0059585668c21f16
TakahashiTakuma <takuma521521@gmail.com>
2020-04-25 08:49:29 +0900

    Add login.
    Using devise.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  get 'welcome_page/welcome'
-  root 'welcome_page#welcome'
+  devise_for :users
+  devise_scope :user do
+    root 'devise/sessions#new'
+    get 'sign_in', to: 'devise/sessions#new'
+  end
+
+  get 'books/index'
 end
