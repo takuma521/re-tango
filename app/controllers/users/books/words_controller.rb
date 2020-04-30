7c210677a8d60002976758d7658e3d56929f564f
TakahashiTakuma <takuma521521@gmail.com>
2020-05-01 00:58:49 +0900

    Add delete button.

--- app/controllers/users/books/words_controller.rb
+++ app/controllers/users/books/words_controller.rb
@@ -1,18 +1,35 @@
 class Users::Books::WordsController < ApplicationController
-  before_action :set_words, only: %w(index)
+  before_action :set_book, only: %w(index create destroy)
+  before_action :set_word, only: %w(destroy)
 
   def index
+    @words = @book.words
   end
 
   def create
-    word = Word.new(book_id: params[:book_id], name: params[:name], translation: params[:translation])
-    word.save!
-    redirect_to user_book_words_path
+    word = @book.words.new(name: params[:name], translation: params[:translation])
+    if word.save!
+      redirect_to user_book_words_path
+    else
+      redirect_to user_book_words_path, status: 422
+    end
+  end
+
+  def destroy
+    if @word.destroy
+      redirect_to user_book_words_path
+    else
+      redirect_to user_book_words_path, status: 422
+    end
   end
 
   private
 
-  def set_words
-    @words = Word.where(book_id: params[:book_id])
+  def set_book
+    @book = Book.find(params[:book_id])
+  end
+
+  def set_word
+    @word = @book.words.find(params[:id])
   end
 end
